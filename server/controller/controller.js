8c7011b9f3557ff9b31d8dadaf0e2329fff2735c
oscarbelm <48737004+oscarbelm@users.noreply.github.com>
2022-10-31 01:48:22 -0600

    Using API

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -31,41 +31,32 @@ exports.create = (req,res)=>{
 
 // // retrieve and return all users/ retrive and return a single user
 exports.find = (req, res)=>{
-    Userdb.find()
-    .then(user =>{
+
+     if(req.query.id){
+         const id = req.query.id;
+
+         Userdb.findById(id)
+             .then(data =>{
+                 if(!data){
+                     res.status(404).send({ message : "Usuario no encontrado con ID:  "+ id})
+                 }else{
+                     res.send(data)
+                 }
+             })
+             .catch(err =>{
+                 res.status(500).send({ message: "Error con el ID:  " + id})
+             })
+
+     }else{
+        Userdb.find()
+        .then(user =>{
         res.send(user)
     })
     .catch(err =>{
         res.status(500).send({message: err.message || "Ocurrio un error"})
     })
 
-//     if(req.query.id){
-//         const id = req.query.id;
-
-//         Userdb.findById(id)
-//             .then(data =>{
-//                 if(!data){
-//                     res.status(404).send({ message : "Not found user with id "+ id})
-//                 }else{
-//                     res.send(data)
-//                 }
-//             })
-//             .catch(err =>{
-//                 res.status(500).send({ message: "Erro retrieving user with id " + id})
-//             })
-
-//     }else{
-//         Userdb.find()
-//             .then(user => {
-//                 res.send(user)
-//             })
-//             .catch(err => {
-//                 res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
-//             })
-    }
-
-    
-// }
+}}
 
 // // Update a new idetified user by user id
 exports.update = (req, res)=>{
--- server/services/render.js
+++ server/services/render.js
@@ -1,19 +1,16 @@
-// const axios = require('axios');
+const axios = require('axios');
 
 
 exports.homeRoutes = (req, res) => {
-    res.render('index');
 //     // Make a get request to /api/users
-//     axios.get('http://localhost:3000/api/users')
-//         .then(function(response){
-//             res.render('index', { users : response.data });
-//         })
-//         .catch(err =>{
-//             res.send(err);
-        }
-
-    
-// }
+     axios.get('http://localhost:3000/api/users')
+         .then(function(response){
+             res.render('index', { users : response.data });
+         })
+         .catch(err =>{
+             res.send(err);
+        })
+ }
 
 exports.add_user = (req, res) =>{
      res.render('add_user');
